d6c4ff2f777d5b02f0001496514357c246c3197a
gkell87 <g_kell87@yahoo.com>
2025-12-24 00:32:44 +0000

    added description of app

---
+++ src/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+// create about page
+function About() {
+    return (
+        <div className = 'about-container'>
+            {/* <h1 className = 'about-heading'>About</h1 */}
+            <p className = 'about-discription'>Welcome to GK's Product Site Page and Portfolio</p>
+            <p className = 'content'>More Place Hoder</p>
+            <p className = 'content'>Placeholder2</p>
+            <p className = 'content'>Placeholder3</p>
+        </div>   
+    );
+}
