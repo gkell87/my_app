1a3b84d5ccbd3d09fbf1564dcc0ddb7bf5ec0183
gkell87 <g_kell87@yahoo.com>
2025-12-31 01:40:54 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import About from './About';
 
 function App() {
   return (
-    <div className="landing-page-container">
-      <div className="background-image"></div>
-      <div className="content"></div>
-       <div className="landing_content"></div>
-       <h1>Welcome to My Digital Portfolio!</h1>
-      <div className = 'divider'></div>
-      <button className="get-started-button">
+    <div className="app-container">
+      <div className={`landing-page`}></div>
+       <div className="background-image"></div>
+       <div className="content"></div>
+        <div className="landing_content"></div>
+        <h1>Welcome to My Digital Portfolio!</h1>
+       <div className = 'divider'></div>
+       <button className="get-started-button">
             Get Started
-      </button>
+       </button>
 </div>
   );
 }
