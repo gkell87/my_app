288e216f005b307381fc1d4875c1151a6cfcc6e8
gkell87 <g_kell87@yahoo.com>
2025-12-31 01:55:04 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import About from './About';
 function App() {
   return (
     <div className="landing-page-container">
-      <div className="content"></div>
-       <div className="landing_content"></div>
-       <h1>Welcome to My Digital Portfolio!</h1>
+      <h1>Welcome to My Digital Portfolio!</h1>
       <div className = 'divider'></div>
       <button className="get-started-button">
             Get Started
