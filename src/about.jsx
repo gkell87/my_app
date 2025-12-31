e2584ed6f72be22a8bc4cff36d34f731271e1e8d
gkell87 <g_kell87@yahoo.com>
2025-12-31 00:46:07 +0000

    css about us edit

--- src/about.jsx
+++ src/about.jsx
@@ -7,6 +7,7 @@ function About() {
         <div className = 'about-container'>
             {/* <h1 className = 'about-heading'>About</h1 */}
             <p className = 'about-discription'>Welcome to GK's Product Site Page and Portfolio</p>
+            {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
             <p className = 'content'>More Place Hoder</p>
             <p className = 'content'>Placeholder2</p>
             <p className = 'content'>Placeholder3</p>
