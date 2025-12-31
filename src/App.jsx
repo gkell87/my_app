fc0602507dff687cc019b55037c9dd36b9db6303
gkell87 <g_kell87@yahoo.com>
2025-12-31 01:01:48 +0000

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="landing-page-container">
-      <div className="background-image"></div>
       <p className="landing-page-description">Welcome to my App!</p>
       <p className="landing-page-content">place holder.</p>
       <p className="landing-page-content">place holder.</p>
       <p className="landing-page-content">place holder.</p>
+      <div className="background-image"></div>
 </div>
   );
 }
